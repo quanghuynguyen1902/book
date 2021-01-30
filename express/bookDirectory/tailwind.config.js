1729cb15a4e908d401b5844ebf94d30999f92885
Huy Nguyen Quang <hoahoc5664@gmail.com>
2021-01-30 17:43:10 +0700

    abc

---
+++ express/bookDirectory/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '760px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
